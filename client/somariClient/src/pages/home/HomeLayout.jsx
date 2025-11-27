12baa5be71f114e140b88de79fbea1e34d8ffaa8
peluojet05 <ibarralui@yahoo.com>
2025-11-27 14:27:30 -0600

    Ignorar .env

--- client/somariClient/src/pages/home/HomeLayout.jsx
+++ client/somariClient/src/pages/home/HomeLayout.jsx
@@ -5,12 +5,14 @@ import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 //import "./home.css";
 import Footer from "./components/Footer";
+import Assistant from "./components/Assistant"
 
 const HomeLayout = () => {
     return (
         <div className="home-layout-container">
             <NavBar />
             <Outlet />
+            <Assistant/>
             <Footer />
         </div>
     );
